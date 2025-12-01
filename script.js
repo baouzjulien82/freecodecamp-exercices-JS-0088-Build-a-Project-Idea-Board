a9e81f3abe4611ebc9b562e6ba15bb93e52e18b3
Julien Baouz <baouzjulien@gmail.com>
2025-12-01 20:20:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const projectStatus = {
-    PENDING : {description: "Pending Execution" },
-    SUCCESS : {description: "Executed Successfully" },
-    FAILURE : {description: "Execution Failed" }
+    PENDING : {description : "Pending Execution" },
+    SUCCESS : {description : "Executed Successfully" },
+    FAILURE : {description : "Execution Failed" }
 };
+
+class ProjectIdea {
+  constructor(title, description) {
+    this.title = title;
+    this.description = description;
+    this.status = projectStatus.PENDING; // valeur pas défaut
+  }
+  updateProjectStatus(newStatus) {
+    this.status = newStatus;
+  }
+}
+
+class ProjectIdeaBoard {
+  constructor(title) {
+    this.title = title;
+    this.ideas = [];
+  }
+  pin()
+  unpin()
+  count()
+  formatToString()
+}
